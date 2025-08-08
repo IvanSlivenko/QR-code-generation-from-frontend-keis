82e1380a19a5e38bc4544d16f80e11cd9e18a0a8
IvanSlivenko <ivan8822@ukr.net>
2025-08-08 18:39:38 +0300

    08-08-2025-18-39

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//! link API
+//img.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${inputValue}`;
+
+
+const wrapper = document.querySelector(".wrapper");
+    form = wrapper.querySelector(".form");
+    input = wrapper.querySelector(".form input");
+    btn = wrapper.querySelector(".form button");
+    img  = wrapper.querySelector(".qr-code img");
+
+let currentValueInput;
