5a89ce9a0e58bf241eb98c835f6b2f011a414929
IvanSlivenko <ivan8822@ukr.net>
2025-08-09 13:15:03 +0300

    09-08-2025-13-14

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ const wrapper = document.querySelector(".wrapper");
     img  = wrapper.querySelector(".qr-code img");
 
 let currentValueInput;
+
+form.addEventListener('submit', (event)=>{
+    
+    event.preventDefault();
+    const inputValue = input.value.trim();
+
+    if(!inputValue || inputValue === currentValueInput) {
+        return
+    }
+
+
+    currentValueInput = inputValue;
+    // console.log('currentValueInput', currentValueInput);
+    btn.textContent = "Відбувається створення QR - Коду";
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${inputValue}`;
+
+    img.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        btn.textContent= "Згенерувати QR - Код"
+    })
+
+    img.addEventListener("error", () =>{
+        alert("Сталась помилка підчас завантаження QR - Коду");
+        location.reload();
+    })
+
+    input.addEventListener("input", () => {
+        if(!input.value.trim() && wrapper.classList.contains("active")){
+            wrapper.classList.remove("active");
+        }
+    })
+})
